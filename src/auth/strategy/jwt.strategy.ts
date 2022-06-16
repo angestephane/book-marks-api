1370332ed55f96d78d7f258f413d8667ffa723aa
stephane boguhe <boguhestephane97@gmail.com>
2022-06-16 10:44:50 +0100

    mise en place des guards pour la protection de nos routes && récupération des données du user authentifié

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,15 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import * as dotenv from 'dotenv';
-import * as mongoose from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../../user/schemas/user.schema';
 
 dotenv.config();
 
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(@InjectModel('User') private userModel: Model<User>) {
     //Initialisation de la strategie
     super({
       //Définit la méthode par laquelle le token sera extrait de la requête
+      //Méthode bearer
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       /**
        * le temps d'expiration à préalablement été défini
@@ -20,5 +23,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
       secretOrKey: process.env.SECRET_KEY,
     });
+  }
+
+  async validate(payload: any) {
+    const sub = payload.sub;
+    //Si aucun utilisateur n'existe dans la base de données une erreur
+    //De non authorisation sera générée
+    const user = await this.userModel.findById(sub, '-_id -__v -hash');
+    return user;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('profile')
+  getProfile(@Req() request: Request) {
+    return { userDatas: request.user };
+  }
+}
